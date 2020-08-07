98ba7d080b816814cb5e2f3b866072488928f357
milenavms <milenavasconcelos.info@gmail.com>
2020-08-07 00:26:14 -0400

    rota 'criar connections' finalizada

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,19 @@
 import express from 'express';
 import ClassControllers from './controllers/ClassesControllers';
+import ConnectionsController from './controllers/ConnectionsController';
 
 const routes = express.Router();
 
 const classControllers = new ClassControllers();
+const connectionsController = new ConnectionsController();
 
 // Cadastro das Classes (Users, Classes e Schedule)
 routes.post('/classes', classControllers.create );
-
-
 // Listagem das Class
 routes.get('/classes', classControllers.index );
+
+
+// Criando as Connections
+routes.post('/connections', connectionsController.create );
 
 export default routes;
